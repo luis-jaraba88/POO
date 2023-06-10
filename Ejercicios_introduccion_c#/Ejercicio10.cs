895bb741da0b061d89b6827d28d37bcd2127124f
luis.jaraba88 <luisjarabatoro88@gmail.com>
2023-06-09 22:47:08 -0500

    ejercicio 10

---
+++ Ejercicios_introduccion_c#/Ejercicio10.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios_introduccion_c_
+{
+    public class Ejercicio10
+    {
+        //Pide 10 números por consola, muestra el resultado por consola de sumar los pares y restarle los impares.
+        private List<int> Numeros = new List<int>();
+        private int Pares { get; set; }
+        private int Impares { get; set; }   
+
+        public void Ejecutar()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"digite numero {i}");
+                var dato = Console.ReadLine();
+                if ( dato != null ) 
+                {
+                    Numeros.Add(int.Parse(dato));
+                }
+                
+            }
+           
+            foreach (int num in Numeros) 
+            {
+                //valida que el numero sea par
+                if (num % 2==0)
+                {
+                    //acumulador para sumar todos los numeros que sean pares
+                    Pares = Pares + num;
+                }
+                else
+                {
+                    //acumula los valores de los numeros que sean impares
+                    Impares= Impares + num;
+                }
+            }
+            var resultado = Pares - Impares;
+            Console.WriteLine($"la resta de los numeros pares menos los numeros impares es {resultado}");
+        }
+
+    }
+
+}
--- Ejercicios_introduccion_c#/Program.cs
+++ Ejercicios_introduccion_c#/Program.cs
@@ -2,20 +2,23 @@
 using Ejercicios_introduccion_c_;
 using System.Reflection.Metadata;
 
-var ejemplo1 = new Ejercicio1();
-ejemplo1.Ejecutar();
+//var ejemplo1 = new Ejercicio1();
+//ejemplo1.Ejecutar();
 
-var ejemplo2 = new Ejercicio2();
-ejemplo2.Ejecutar();
+//var ejemplo2 = new Ejercicio2();
+//ejemplo2.Ejecutar();
 
-var ejemplo3 = new Ejercicio3();
-ejemplo3.Ejecutar();
+//var ejemplo3 = new Ejercicio3();
+//ejemplo3.Ejecutar();
 
-var ejemplo6 = new Ejercicio6();
-ejemplo6.Ejecutar();
+//var ejemplo6 = new Ejercicio6();
+//ejemplo6.Ejecutar();
 
-var ejemplo4 = new Ejercicio4();
-ejemplo4.Ejecutar();
+//var ejemplo4 = new Ejercicio4();
+//ejemplo4.Ejecutar();
+
+var ejemplo10= new Ejercicio10();
+ejemplo10.Ejecutar();
 
 
 
